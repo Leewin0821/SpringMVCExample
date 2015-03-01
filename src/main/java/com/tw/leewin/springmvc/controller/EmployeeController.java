55f7f10b3eae6c92c27cb92ddf87c622aef04123
lwzhang <lwzhang@thoughtworks.com>
2015-03-01 21:14:38 +0800

    Add Controller

---
+++ src/main/java/com/tw/leewin/springmvc/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.tw.leewin.springmvc.controller;
+
+import com.tw.leewin.springmvc.service.EmployeeManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by lwzhang on 3/1/15.
+ */
+
+@Controller
+@RequestMapping("/employee-module")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeManager employeeManager;
+
+    @RequestMapping(value = "/getAllEmployees", method = RequestMethod.GET)
+    public String getAllEmployees(Model model){
+        model.addAttribute("employees", employeeManager.getAllEmployees());
+        return "employeeListDisplay";
+    }
+}
--- src/main/webapp/WEB-INF/views/employeeListDisplay.jsp
+++ src/main/webapp/WEB-INF/views/employeeListDisplay.jsp
@@ -13,7 +13,25 @@
 <head>
     <title>Spring MVC Hello World</title>
 </head>
+
 <body>
+
     <h2>All Employees In System</h2>
+
+    <table border="1">
+        <tr>
+            <th>Employee Id</th>
+            <th>Employee First Name</th>
+            <th>Employee Last Name</th>
+        </tr>
+        <c:forEach items="${employees}" var="employee">
+            <tr>
+                <td>${employee.id}</td>
+                <td>${employee.firstName}</td>
+                <td>${employee.lastName}</td>
+            </tr>
+        </c:forEach>
+    </table>
+
 </body>
 </html>
