90eb1ef91998098df83936f66e13f9b1f7568cdf
lwzhang <lwzhang@thoughtworks.com>
2015-03-02 13:04:52 +0800

    Finished Jetty Configuration

---
+++ src/main/java/com/tw/leewin/springmvc/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.tw.leewin.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by lwzhang on 3/2/15.
+ */
+@Controller
+public class WelcomeController {
+
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String welcome(){
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lwzhang
+  Date: 3/2/15
+  Time: 9:49 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index Page Title</title>
+</head>
+<body>
+    <h2>Welcome To Index Page</h2>
+</body>
+</html>
